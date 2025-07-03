2ca1c20d8f90bb36abd4268408ade273651f3e40
최형원 <choehyungwon@choehyeong-won-ui-MacBookAir.local>
2025-07-03 14:24:16 +0900

    Initial commit

---
+++ src/main/java/com/rephoto/rephoto_api/RephotoApplication.java
@@ -0,0 +1,13 @@
+package com.rephoto.rephoto_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RephotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RephotoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rephoto/rephoto_api/RephotoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rephoto.rephoto_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RephotoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

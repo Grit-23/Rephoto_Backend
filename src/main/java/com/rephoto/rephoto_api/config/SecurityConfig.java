e241de6579e0b502bf386f68ebb221beae0cc6ab
최형원 <choehyungwon@choehyeong-won-ui-MacBookAir.local>
2025-07-24 01:23:43 +0900

    fix: S3 upload jwt 인증 제외

--- src/main/java/com/rephoto/rephoto_api/config/SecurityConfig.java
+++ src/main/java/com/rephoto/rephoto_api/config/SecurityConfig.java
@@ -26,7 +26,9 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/join",
+                        .requestMatchers(
+                                "/api/files/upload",
+                                "/api/join",
                                 "/api/login",
                                 "/api/kakao/login",
                                 "/swagger-ui.html",

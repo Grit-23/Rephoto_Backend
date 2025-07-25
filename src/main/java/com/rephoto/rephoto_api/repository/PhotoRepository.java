b6d9b4739cbc310b09e6260b7000c8073410212a
최형원 <choehyungwon@choehyeong-won-ui-MacBookAir.local>
2025-07-25 21:16:16 +0900

    fix: hash error

--- src/main/java/com/rephoto/rephoto_api/repository/PhotoRepository.java
+++ src/main/java/com/rephoto/rephoto_api/repository/PhotoRepository.java
@@ -30,11 +30,11 @@ public interface PhotoRepository extends JpaRepository<Photo, Long> {
     //List<Photo> findPhotosByUserIdAndTagId(Long userId, Long tagId);
     //앨범 상세 정보 -> 앨범에 저장된 사진 리스트
 
-    boolean existsByUserUserIdAndHash(Long userId, String hash);
-
     // 지도 범위 내의 사진들을 가져오기 위한 메서드
     List<Photo> findByUser_UserIdAndLatitudeBetweenAndLongitudeBetween(
             Long userId, Double minLat, Double maxLat, Double minLng, Double maxLng
     );
+
+    boolean existsByUserUserIdAndImageUrl(Long userId, String imageUrl);
 
 }

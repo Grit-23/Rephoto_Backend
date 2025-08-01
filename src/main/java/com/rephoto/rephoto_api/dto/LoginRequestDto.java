a92fb8c9efd807c6142968bf93ef0d940968b834
최형원 <choehyungwon@choehyeong-won-ui-MacBookAir.local>
2025-08-01 17:14:25 +0900

    fix: change dto for test

--- src/main/java/com/rephoto/rephoto_api/dto/LoginRequestDto.java
+++ src/main/java/com/rephoto/rephoto_api/dto/LoginRequestDto.java
@@ -1,9 +1,11 @@
 package com.rephoto.rephoto_api.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 @Schema(description = "로그인 요청 DTO")
 public class LoginRequestDto {
 

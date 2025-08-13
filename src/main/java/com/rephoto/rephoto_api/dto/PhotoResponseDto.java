a8e3f5e5c055da95644c3f287764b91704c5016a
onowoey <cg10046@naver.com>
2025-08-13 10:15:02 +0900

    사진 조회 시 tag 반환

--- src/main/java/com/rephoto/rephoto_api/dto/PhotoResponseDto.java
+++ src/main/java/com/rephoto/rephoto_api/dto/PhotoResponseDto.java
@@ -1,12 +1,15 @@
 package com.rephoto.rephoto_api.dto;
 
 import com.rephoto.rephoto_api.domain.Photo;
+import com.rephoto.rephoto_api.domain.PhotoTag;
+import com.rephoto.rephoto_api.domain.Tag;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Setter
@@ -35,6 +38,9 @@ public class PhotoResponseDto {
     @Schema(description = "사진 파일 이름", example = "IMG_20250720_150000.jpg")
     private String fileName;
 
+    @Schema(description = "사진 태그 모음", example = "(바다,하늘)")
+    private List<PhotoTag> tags;
+
     public static PhotoResponseDto fromEntity(Photo photo) {
         return PhotoResponseDto.builder()
                 .photoId(photo.getPhotoId())
@@ -44,6 +50,7 @@ public class PhotoResponseDto {
                 .longitude(photo.getLongitude())
                 .createdAt(photo.getCreatedAt())
                 .fileName(photo.getFileName())
+                .tags(photo.getPhotoTags())
                 .build(); // 필요한 경우 latitude 등도 매핑 가능
     }
 }

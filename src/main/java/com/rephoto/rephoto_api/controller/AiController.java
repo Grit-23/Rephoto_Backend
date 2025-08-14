6e2e23261c5beb04c7ed3b8bddd82f51cddfc63f
최형원 <choehyungwon@choehyeong-won-ui-MacBookAir.local>
2025-08-15 04:06:14 +0900

    fix: /caption/generate swagger 추가

--- src/main/java/com/rephoto/rephoto_api/controller/AiController.java
+++ src/main/java/com/rephoto/rephoto_api/controller/AiController.java
@@ -3,6 +3,7 @@ package com.rephoto.rephoto_api.controller;
 import com.rephoto.rephoto_api.dto.ImageCaptionRequestDto;
 import com.rephoto.rephoto_api.dto.ImageCaptionResponse;
 import com.rephoto.rephoto_api.service.AiService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +19,7 @@ public class AiController {
     private final AiService aiService;
 
     @PostMapping("/caption/generate")
+    @Operation(summary = "URL로 설명 생성 요청", description = "S3 url 입력하여 요청 전달 (기본)")
     public ResponseEntity<ImageCaptionResponse> generateCaptionByUrl(
             @RequestBody ImageCaptionRequestDto imageCaptionRequestDto) {
 
@@ -26,6 +28,7 @@ public class AiController {
     }
 
     @PostMapping("/caption/generate-by-file")
+    @Operation(summary = "FILE로 설명 생성 요청", description = "파일 직접 업로드하여 요청 전달 (임시)")
     public ResponseEntity<ImageCaptionResponse> generateCaptionByFile(
             @RequestParam("file")MultipartFile file
             ){

0bf437ef7bb94758a15d16292e56967dfc7d354d
onowoey <cg10046@naver.com>
2025-08-16 00:13:47 +0900

    충돌 수정

--- src/main/java/com/rephoto/rephoto_api/exception/ErrorCode.java
+++ src/main/java/com/rephoto/rephoto_api/exception/ErrorCode.java
@@ -54,6 +54,7 @@ public enum ErrorCode {
     SEARCH_QUERY_REQUIRED(HttpStatus.BAD_REQUEST, "검색어가 비어있습니다."),
     TOO_MANY_TAGS(HttpStatus.BAD_REQUEST, "태그는 최대 20개까지 입력할 수 있습니다."),
     SEARCH_TAGS_REQUIRED(HttpStatus.BAD_REQUEST, "유효한 태그가 없습니다. '#'으로 시작하는 태그를 입력해주세요."),
+    VECTOR_DIMENSION_MISMATCH(HttpStatus.BAD_REQUEST, "벡터 차원이 다릅니다."),
     
     //태그 관련
     TAG_NOT_FOUND(HttpStatus.NOT_FOUND,"해당 태그가 존재하지 않습니다"),
